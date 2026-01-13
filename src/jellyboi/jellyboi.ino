cc08908b62946bd09c569d83b70d79bdf28fd531
Richard van 't Hof <hello@therichard.space>
2026-01-13 16:28:32 +0100

    Added led array support

--- src/jellyboi/jellyboi.ino
+++ src/jellyboi/jellyboi.ino
@@ -7,14 +7,19 @@ const int en1Pin = 10;
 const int in1Pin = 9;
 const int in2Pin = 8;
 
-const int jellySpeed = 255; // Your requested speed
+//Light pins
+const int led = 3;
 
+const int jellySpeed = 255; // Your requested speed
+  
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(en1Pin, OUTPUT);
   pinMode(in1Pin, OUTPUT);
   pinMode(in2Pin, OUTPUT);
+
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
@@ -28,34 +33,37 @@ void loop() {
   long duration = pulseIn(echoPin, HIGH);
   int distance = duration * 0.034 / 2;
 
-  if (distance > 0 && distance <= 5) {
+  if (distance > 0 && distance <= 100) {
     
     // --- JELLYFISH "UP" PHASE (Total ~1 second) ---
-    // We do 3 quick pulses of: Push Forward -> Slight Back
-    for (int i = 0; i < 3; i++) {
+    // We do 2 quick pulses of: Push Forward -> Slight Back
+    for (int i = 0; i < 2; i++) {
       // Push Up
       digitalWrite(in1Pin, HIGH);
       digitalWrite(in2Pin, LOW);
       analogWrite(en1Pin, jellySpeed);
-      delay(250); 
+      delay(200); 
       
       // Slight Down (The "Relax")
       digitalWrite(in1Pin, LOW);
       digitalWrite(in2Pin, HIGH);
       analogWrite(en1Pin, 100); // Lower speed for the relax
-      delay(100);
+      delay(150);
+
+      digitalWrite(led, HIGH);
     }
 
     // --- RESET PHASE (Back down for 1 second) ---
     digitalWrite(in1Pin, LOW);
     digitalWrite(in2Pin, HIGH);
     analogWrite(en1Pin, jellySpeed);
-    delay(1000);
+    delay(1200);
 
     // --- STOP ---
     digitalWrite(in1Pin, LOW);
     digitalWrite(in2Pin, LOW);
     analogWrite(en1Pin, 0);
+    digitalWrite(led, LOW);
   }
   
   delay(50); 
